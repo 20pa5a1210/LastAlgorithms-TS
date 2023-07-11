ae2f66774168f8667eeb9a008ec554c759fcccb2
20pa5a1210 <20pa5a1210@vishnu.edu.in>
2023-07-11 22:43:53 +0530

    two crystal balls problem

--- src/day2/Queue.ts
+++ src/day2/Queue.ts
@@ -1,18 +1,18 @@
 export default class Queue<T> {
     public length: number;
 
-    
+
 
     constructor() {
     }
 
     enqueue(item: T): void {
 
-}
+    }
     deque(): T | undefined {
 
-}
+    }
     peek(): T | undefined {
 
-}
+    }
 }
--- src/day2/TwoCrystalBalls.ts
+++ src/day2/TwoCrystalBalls.ts
@@ -1,24 +1,22 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
 
-  const jmpAmount = Math.floor(Math.sqrt(breaks.length));
-  let i = jmpAmount;
+  const jmpAmout = Math.floor(Math.sqrt(breaks.length));
+  let jmp = jmpAmout;
 
-  for(;i<breaks.length;i+=jmpAmount){
-    if(breaks[i]){
+  while (breaks[jmp] === false) {
+    jmp += jmpAmout;
+    if (breaks[jmp] === true) {
       break;
     }
   }
 
-  i -= jmpAmount;
-  
-  for (let j=0;j<jmpAmount && i < breaks.length; ++j, ++i){
-    if(breaks[i]){
+  let i = jmp - jmpAmout;
+
+  for (let j = 0; j < jmpAmout && i < breaks.length; j++) {
+    if (breaks[i] === true) {
       return i;
     }
+    i++;
   }
-
   return -1;
-
-
-
 }
