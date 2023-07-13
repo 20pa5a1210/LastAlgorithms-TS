bb7d33c74d4a98a500802321748adf191baff4d9
20pa5a1210 <20pa5a1210@vishnu.edu.in>
2023-07-13 22:16:58 +0530

    feat: queue implementation

--- src/day2/Queue.ts
+++ src/day2/Queue.ts
@@ -1,18 +1,39 @@
+type Qnode<T> = {
+    value: T;
+    next?: Qnode<T>;
+}
 export default class Queue<T> {
     public length: number;
-
-
+    private head: Qnode<T> | undefined;
+    private tail: Qnode<T> | undefined;
 
     constructor() {
+        this.head = this.tail = undefined;
+        this.length = 0;
     }
 
     enqueue(item: T): void {
+        const node = { value: item } as Qnode<T>;
+        this.length++;
+        if (!this.tail) {
+            this.tail = this.head = node;
+            return;
+        }
+        this.tail.next = node;
+        this.tail = node;
 
     }
     deque(): T | undefined {
+        if (!this.head) return undefined;
+        this.length--;
+        const head = this.head;
+        this.head = this.head.next;
+
+        head.next = undefined;
+        return head.value;
 
     }
     peek(): T | undefined {
-
+        return this.head?.value;
     }
 }
--- src/day2/SinglyLinkedList.ts
+++ src/day2/SinglyLinkedList.ts
@@ -1,27 +1,61 @@
+type Node<T> = {
+    value: T;
+    next: Node<T> | null;
+}
 export default class SinglyLinkedList<T> {
     public length: number;
+    private head: Node<T> | null;
 
-    
+
+
 
     constructor() {
+        this.length = 0;
+        this.head = null;
     }
 
     prepend(item: T): void {
 
-}
+    }
     insertAt(item: T, idx: number): void {
 
-}
+    }
     append(item: T): void {
+        if (this.head === null) {
+            this.head = {
+                value: item,
+                next: null
+            }
 
-}
+        }
+        else {
+            let current: Node<T> = this.head;
+            while (current.next !== null) {
+                current = current.next;
+            }
+            current.next = {
+                value: item,
+                next: null
+            }
+        }
+    }
     remove(item: T): T | undefined {
+        let current: Node<T> | null = this.head;
+        while (current !== null) {
+            if (current.next?.value === item) {
+                // TODO: remove item
 
-}
+                current.next = null;
+
+            }
+            current = current.next;
+        }
+        return undefined;
+    }
     get(idx: number): T | undefined {
 
-}
+    }
     removeAt(idx: number): T | undefined {
 
-}
+    }
 }
